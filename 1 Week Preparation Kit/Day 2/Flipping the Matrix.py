c3bed0d7d822bdcb4a31461db7ceb318079517dd
Nabil Khalil <89206827+nkhalil98@users.noreply.github.com>
2023-03-16 12:45:05 -0400

    Update Flipping the Matrix.py

--- 1 Week Preparation Kit/Day 2/Flipping the Matrix.py
+++ 1 Week Preparation Kit/Day 2/Flipping the Matrix.py
@@ -3,5 +3,5 @@ def flippingMatrix(matrix):
     total = 0
     for i in range(n//2):
         for j in range(n//2):
-            total += max(matrix[i][j], matrix[i][-j-1], matrix[-i-1][j], matrix[-i-1][-j-1])
+            total += max(matrix[i][j], matrix[i][-(j+1)], matrix[-(i+1)][j], matrix[-(i+1)][-(j+1)])
     return total
